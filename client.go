8e154f290d77e45a46280758042bab51c3e9601b
SuLinXin66 <15210204903@163.com>
2022-07-27 21:51:33 +0800

    更新客户端RPC

--- client.go
+++ client.go
@@ -6,3 +6,15 @@ import "net/rpc"
 type DeployApplicationPluginRPC struct {
 	client *rpc.Client
 }
+
+func (d *DeployApplicationPluginRPC) Info() *PluginInfo {
+	var resp *PluginInfo
+	err := d.client.Call("Plugin.Info", nil, &resp)
+	if err != nil {
+		// You usually want your interfaces to return errors. If they don't,
+		// there isn't much other choice here.
+		panic(err)
+	}
+
+	return resp
+}
