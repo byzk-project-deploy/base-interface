ef04dfec8faac56ea43979b4cff8e95758edd2d5
SuLinXin66 <15210204903@163.com>
2022-07-27 20:38:45 +0800

    增加插件测试接口方法

--- interface.go
+++ interface.go
@@ -1,5 +1,17 @@
 package rpcinterfaces
 
+// PluginInfo 插件
+type PluginInfo struct {
+	// Name 插件名称
+	Name string
+	// Desc 插件描述
+	Desc string
+	// Type 插件类别
+	Type string
+}
+
 // DeployApplicationPluginInterface 部署应用的插件接口
 type DeployApplicationPluginInterface interface {
+	// Info 插件信息
+	Info() *PluginInfo
 }
